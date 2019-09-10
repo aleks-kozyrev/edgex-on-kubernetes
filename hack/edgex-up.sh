e26c6d23ef7cf109cdae5ca32e73bddc7b35f97e
Alex Kozyrev <alex.kozyrev@windriver.com>
2019-09-10 11:57:37 -0400

    Update edgex-up.sh

--- hack/edgex-up.sh
+++ hack/edgex-up.sh
@@ -6,7 +6,9 @@ EDGEX_ROOT=$(dirname "${BASH_SOURCE}")/..
 
 function create_services {
 
+  kubectl create -f "${EDGEX_ROOT}/services/files-service.yaml"
   kubectl create -f "${EDGEX_ROOT}/services/consul-service.yaml"
+  kubectl create -f "${EDGEX_ROOT}/services/config-seed-service.yaml"
   kubectl create -f "${EDGEX_ROOT}/services/mongo-service.yaml"
   kubectl create -f "${EDGEX_ROOT}/services/logging-service.yaml"
   kubectl create -f "${EDGEX_ROOT}/services/notifications-service.yaml"
@@ -17,33 +19,42 @@ function create_services {
   kubectl create -f "${EDGEX_ROOT}/services/export-client-service.yaml"
   kubectl create -f "${EDGEX_ROOT}/services/export-distro-service.yaml"
   kubectl create -f "${EDGEX_ROOT}/services/rulesengine-service.yaml"
+  kubectl create -f "${EDGEX_ROOT}/services/device-virtual-service.yaml"
   kubectl create -f "${EDGEX_ROOT}/services/device-random-service.yaml"
 }
 
 function create_deployments {
 
+  kubectl create -f "${EDGEX_ROOT}/deployments/files-deployment.yaml"
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/consul-deployment.yaml"
   sleep 10
+  kubectl create -f "${EDGEX_ROOT}/deployments/config-seed-deployment.yaml"
+  sleep 10
   kubectl create -f "${EDGEX_ROOT}/deployments/mongo-deployment.yaml"
-  sleep 10
+  sleep 3
+  kubectl delete -f "${EDGEX_ROOT}/deployments/config-seed-deployment.yaml"
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/logging-deployment.yaml"
-  sleep 10
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/notifications-deployment.yaml"
-  sleep 10
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/metadata-deployment.yaml"
-  sleep 10
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/data-deployment.yaml"
-  sleep 10
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/command-deployment.yaml"
-  sleep 10
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/scheduler-deployment.yaml"
-  sleep 10
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/export-client-deployment.yaml"
-  sleep 10
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/export-distro-deployment.yaml"
-  sleep 10
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/rulesengine-deployment.yaml"
-  sleep 10
+  sleep 3
+  kubectl create -f "${EDGEX_ROOT}/deployments/device-virtual-deployment.yaml"
+  sleep 3
   kubectl create -f "${EDGEX_ROOT}/deployments/device-random-deployment.yaml"
 }
 
